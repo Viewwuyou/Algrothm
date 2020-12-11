1ad6e658f6d48a4401b2951029acde18847066c0
viewwuyou <viewwuyou@gmail.com>
2020-12-11 20:16:55 +0800

    构建了图的邻接表表示法，adj函数返回一个某个顶点的邻接顶点集。

---
+++ src/com/viewwuyou/algrothm/Graph/AdjGraph.java
@@ -0,0 +1,43 @@
+package com.viewwuyou.algrothm.Graph;
+
+import java.util.ArrayList;
+
+public class AdjGraph extends AbstractGraph {
+
+    private final int V;
+    private int E;
+    private ArrayList<Integer>[] adj;
+
+    @SuppressWarnings("unchecked")
+    public AdjGraph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (ArrayList<Integer>[]) new ArrayList[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new ArrayList<>();
+        }
+    }
+
+
+    @Override
+    public int V() {
+        return this.V;
+    }
+
+    @Override
+    public int E() {
+        return this.E;
+    }
+
+    @Override
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+        adj[w].add(v);
+        E++;
+    }
+
+    @Override
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+}
