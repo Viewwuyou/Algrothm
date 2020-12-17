f3a6176d8e8cf5fae3720d86e6bf1e0064ead023
viewwuyou <viewwuyou@gmail.com>
2020-12-17 13:25:02 +0800

    实现了广度优先搜索，并加以注释和测试

--- src/com/viewwuyou/algrothm/Graph/BreadthFirstSearch.java
+++ src/com/viewwuyou/algrothm/Graph/BreadthFirstSearch.java
@@ -1,7 +1,6 @@
 package com.viewwuyou.algrothm.Graph;
 
 import java.util.LinkedList;
-import java.util.Queue;
 
 public class BreadthFirstSearch {
 
@@ -17,8 +16,8 @@ public class BreadthFirstSearch {
      * 其实也很好理解，由于深度优先搜索要求对邻节点优先搜索，因此需要不断遍历到底，再挨个返回，这是满足栈这个数据结构的
      * 但是广度优先搜索要求先搜索相对与一个父节点的所有兄弟节点，因此需要先将搜索过的节点遍历，也就时先进先出，这是满足队列这个数据结构的
      * 由于用了队列，这里不能再使用递归这个隐式栈结构，直接使用循环进行标记。
-     * @param G
-     * @param s
+     * @param G 指定的图
+     * @param s 开始搜索的节点
      */
     public void bfs(Graph G, int s) {
         LinkedList<Integer> queue = new LinkedList<>();
